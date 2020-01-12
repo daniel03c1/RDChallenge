d0d317314285aef6291cc371e7272efbe98c4d5e
Daniel <daniel03c195@gmail.com>
2020-01-12 08:53:06 +0000

    added model and util files

---
+++ tf_codes/models.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import *
+import keras.backend as K
+
+
+def dense_net_based_model(input_shape,
+                          n_classes,
+                          n_layer_per_block,
+                          growth_rate,
+                          normalization='layernorm',
+                          activation='softmax',
+                          se=False,
+                          *args, **kwargs):
+    """
+    :param input_shape: LIST of length 3
+        [height, width, chan]
+    :param n_classes: INT
+        the number of output classes
+    :param n_layer_per_block: LIST of length (n_block)
+        each integer indicates the number of layer per block
+    :param growth_rate: INT
+        the growth rate of dense block
+    :param normalization: ('layernorm', 'batchnorm', 'none')
+    :param activation: STR
+        activation function of the final layer
+
+    :return: Keras Model
+    """
+    model_input = Input(shape=input_shape)
+    n_block = len(n_layer_per_block)
+
+    # FRONT
+    if normalization == 'batchnorm':
+        x = BatchNormalization()(model_input)
+    elif normalization == 'layernorm':
+        x = LayerNormalization()(model_input)
+    else:
+        x = model_input
+    x = Conv2D(filters=growth_rate*2,
+               kernel_size=[7, 7],
+               strides=[1, 1],
+               padding='same',
+               *args, **kwargs)(x)
+    x = MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
+
+    # MIDDLE
+    for i in range(n_block - 1):
+        x = dense_block(n_layer=n_layer_per_block[i],
+                        growth_rate=growth_rate,
+                        se=se,
+                        *args, **kwargs)(x)
+        x = dense_net_conv2d(filters=growth_rate * 4,
+                             kernel_size=[1, 1],
+                             padding='same',
+                             *args, **kwargs)(x)
+        x = AveragePooling2D()(x)
+
+    # FINAL
+    x = dense_block(n_layer=n_layer_per_block[-1],
+                    growth_rate=growth_rate,
+                    se=se,
+                    *args, **kwargs)(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(units=growth_rate*16, activation='relu', *args, **kwargs)(x)
+    x = Dense(units=n_classes, activation=activation)(x)
+
+    model = tf.keras.Model(inputs=model_input, outputs=x)
+    return model
+
+
+def dense_net_conv2d(*args, **kwargs):
+    def _dense_net_conv2d(tensor):
+        x = BatchNormalization()(tensor)
+        x = ReLU()(x)
+        x = Conv2D(*args, **kwargs)(x)
+        return x
+    return _dense_net_conv2d
+
+
+def dense_block(n_layer, growth_rate, se=False, *args, **kwargs):
+    def _dense_block(tensor):
+        x = tensor
+        if se:
+            x = squeeze_excitation(reduction=2, axis=3, *args, **kwargs)(x)
+            x = squeeze_excitation(reduction=2, axis=1, *args, **kwargs)(x)
+
+        for _ in range(n_layer):
+            y = Dropout(0.25)(x)
+            y = dense_net_conv2d(filters=growth_rate,
+                                 kernel_size=[1, 1],
+                                 padding='same',
+                                 *args, **kwargs)(y)
+            y = dense_net_conv2d(filters=growth_rate,
+                                 kernel_size=[3, 3],
+                                 padding='same',
+                                 *args, **kwargs)(y)
+            x = Concatenate(axis=-1)([x, y])
+
+        return x
+    return _dense_block
+
+
+def squeeze_excitation(reduction=4, axis=1, *args, **kwargs):
+    def _se(tensor):
+        assert len(tensor.shape) == 4
+        x = K.stop_gradient(tensor)
+        reduce_axis = tuple([i for i in range(1, 4) if i != axis])
+        mean = Lambda(lambda x: K.mean(x, axis=reduce_axis))(x)
+        std = Lambda(lambda x: K.std(x, axis=reduce_axis))(x)
+        squeeze = Concatenate(axis=-1)([mean, std])
+
+        excitation = Dense(tensor.shape[axis] // reduction,
+                           activation='relu', *args, **kwargs)(squeeze)
+        excitation = Dense(tensor.shape[axis],
+                           activation='tanh', *args, **kwargs)(excitation)
+        excitation = Reshape(
+            [1 if i != axis else tensor.shape[i] for i in range(1, 4)])(excitation)
+
+        return Multiply()([tensor, excitation])
+    return _se
---
+++ tf_codes/utils.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import numpy as np
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+
+def azimuth_to_classes(azimuth, n_classes, one_hot=True):
+    assert n_classes in [2, 10, 11]
+
+    if n_classes == 2:
+        classes = np.not_equal(azimuth, -1).astype(np.int32) 
+    elif n_classes == 10:
+        classes = azimuth // 20
+    else:
+        mask = np.equal(azimuth, -1).astype(np.int32)
+        classes = mask * 10 + (1 - mask) * azimuth // 20
+    if one_hot:
+        return np.eye(n_classes, dtype=np.float32)[classes]
+    else:
+        return classes
+
+
+def class_to_azimuth(classes):
+    mask = tf.cast(tf.equal(classes, 10), classes.dtype)
+    azimuth = mask * -1 + (1 - mask) * classes * 20
+    return azimuth
+
+
+def normalize_spec(x, log=True, chan=(0, 1)):
+    if log:
+        x = x.copy()
+        x[:, :, :, chan] = np.log(x[:, :, :, chan] + 1e-8)
+    return x
+
+
+def augment(mask=True, equalizer=True):
+    def _aug(x, y):
+        if mask:
+            x = freq_mask(x)
+            x = time_mask(x)
+        if equalizer:
+            x = random_equalizer(x)
+        return x, y
+    return _aug
+
+
+def freq_mask(spec, max_mask_size=8, mask_num=2):
+    freq, time, _ = spec.shape
+    mask = tf.ones(shape=(freq, 1, 1), dtype=tf.float32)
+
+    for i in range(mask_num):
+        size = tf.random.uniform([], minval=0, maxval=max_mask_size, dtype=tf.int32)
+        offset = tf.random.uniform([], minval=0, maxval=freq-size, dtype=tf.int32)
+
+        mask *= tf.concat((tf.ones(shape=(offset, 1, 1)),
+                           tf.zeros(shape=(size, 1, 1)),
+                           tf.ones(shape=(freq-size-offset, 1, 1))),
+                           0)
+    spec = spec * mask
+    return tf.cast(spec, dtype=tf.float32)
+
+
+def time_mask(spec, max_mask_size=32, mask_num=2):
+    freq, time, _ = spec.shape
+    mask = tf.ones(shape=(1, time, 1), dtype=tf.float32)
+
+    for i in range(mask_num):
+        size = tf.random.uniform([], minval=0, maxval=max_mask_size, dtype=tf.int32)
+        offset = tf.random.uniform([], minval=0, maxval=time-size, dtype=tf.int32)
+
+        mask *= tf.concat((tf.ones(shape=(1, offset, 1)),
+                           tf.zeros(shape=(1, size, 1)),
+                           tf.ones(shape=(1, time-size-offset, 1))),
+                           1)
+    spec = spec * mask
+    return tf.cast(spec, dtype=tf.float32)
+
+
+def random_equalizer(spec):
+    freq, time, chan = spec.shape
+    
+    def _gen(maxval):
+        left = tf.random.uniform([], maxval=np.pi*2, dtype=tf.float32)
+        right = tf.random.uniform([], maxval=np.pi*3, dtype=tf.float32)
+        weight = tf.random.uniform([], maxval=maxval, dtype=tf.float32)
+        return tf.math.cos(tf.linspace(left, left+right, freq)) * weight
+
+    equalizer = _gen(0.2) + _gen(0.2) + 1
+    equalizer = tf.expand_dims(tf.expand_dims(equalizer, 1), 1)
+    equalizer = tf.concat((tf.tile(equalizer, [1, 1, chan//2]),
+                           tf.ones(shape=(freq, 1, chan//2))),
+                           2)
+    return spec * equalizer
+
+
+def make_dataset(x, y, n_proc, batch_per_node, train=False, **kwargs):
+    dataset = tf.data.Dataset.from_tensor_slices((x, y)).cache()
+    if train:
+        dataset = dataset.map(augment(**kwargs), num_parallel_calls=AUTOTUNE)
+        dataset = dataset.repeat().shuffle(buffer_size=5000)
+    return dataset.batch(batch_per_node, drop_remainder=True).prefetch(AUTOTUNE)
+
